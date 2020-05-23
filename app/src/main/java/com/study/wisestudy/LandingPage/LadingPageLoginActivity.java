32076d8214ea05b5e6360a666fc3e38b37dc6302
GyuJin-Cho <jjabkk6@naver.com>
2020-05-24 00:02:44 +0900

    feat(#4): 랜딩페이지 UI 구현

--- app/src/main/java/com/study/wisestudy/LandingPage/LadingPageLoginActivity.java
+++ app/src/main/java/com/study/wisestudy/LandingPage/LadingPageLoginActivity.java
@@ -1,4 +1,27 @@
 package com.study.wisestudy.LandingPage;
 
-public class LadingPageLoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.study.wisestudy.MyInformation.InsertProfileActivity;
+import com.study.wisestudy.R;
+
+public class LadingPageLoginActivity extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page_login);
+        Button Login = (Button) findViewById(R.id.Login_Btn);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),InsertProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
