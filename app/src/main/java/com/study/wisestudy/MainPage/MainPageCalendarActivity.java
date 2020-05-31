c07ce8e3a6dced360aa323d1dc5cf864fe5b8fb3
이동우 <>
2020-05-31 18:24:59 +0900

    fact(#13) : 메인화면 상단 툴바 UI 구현 툴바 내비게이션뷰 UI 구현

--- app/src/main/java/com/study/wisestudy/MainPage/MainPageCalendarActivity.java
+++ app/src/main/java/com/study/wisestudy/MainPage/MainPageCalendarActivity.java
@@ -1,30 +1,80 @@
 package com.study.wisestudy.MainPage;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.study.wisestudy.MainPage.RecyclerView.RecyclerViewAdapter;
 import com.study.wisestudy.MainPage.RecyclerView.RecyclerViewData;
 import com.study.wisestudy.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainPageCalendarActivity extends AppCompatActivity {
-    private ImageView menuImage;
+public class MainPageCalendarActivity extends AppCompatActivity  {
+
     private ArrayList<RecyclerViewData> recyclerDataList;
     private RecyclerView recyclerView;
     private LinearLayoutManager manager;
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    private ExpandableListView expandableListView;
+    private NavigationDrawerPageExpandListviewAdapter expandListviewAdapter;
+    private ArrayList<String> joinedGroup = new ArrayList<>();
+    private Map<String,ArrayList<String>> joinedGroupChild = new HashMap<>();
+
+    private View navigationHeader;
+    private ImageButton drawerNavigationClose;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendarpage);
 
+        toolbar = findViewById(R.id.toolBarId);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
+
+
+        drawerLayout = findViewById(R.id.mainPageId);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,0,0);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        expandableListView = findViewById(R.id.joinedGroupListId);
+        navigationView = findViewById(R.id.navigationviewId);
+        navigationHeader = navigationView.getHeaderView(0);
+        drawerNavigationClose = navigationHeader.findViewById(R.id.navigationviewCloseId);
+        drawerNavigationClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+        joinedData();
 
         this.InitializeData();
         recyclerView = findViewById(R.id.recyclerId);
@@ -33,20 +83,66 @@ public class MainPageCalendarActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(new RecyclerViewAdapter(recyclerDataList));
 
-        menuImage = findViewById(R.id.menuId);
-        menuImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),MainPageMenuActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 
+    private void joinedData(){
+
+        ArrayList<String> joinedList = new ArrayList<>();
+
+        joinedGroup.add("가입한 그룹");
+
+
+        joinedList.add("프로그라피");
+        joinedList.add("SCH");
+
+        joinedGroupChild.put(joinedGroup.get(0),joinedList);
+        expandListviewAdapter = new NavigationDrawerPageExpandListviewAdapter(joinedGroup,joinedGroupChild,this);
+        expandableListView.setAdapter(expandListviewAdapter);
+
+    }
     private void InitializeData() {
         recyclerDataList = new ArrayList<>();
         recyclerDataList.add(new RecyclerViewData(R.color.colorAccent,"7일","안양 커피빈","오후 2시"));
         recyclerDataList.add(new RecyclerViewData(R.color.colorPrimary,"14일","안양 스터디룸","오후 1시"));
         recyclerDataList.add(new RecyclerViewData(R.color.colorPrimaryDark,"21일","범계 커피빈","오후 3시"));
     }
+
+    @Override
+    public void onBackPressed() {
+       if(drawerLayout.isDrawerOpen(GravityCompat.START))
+       {
+           drawerLayout.closeDrawer(GravityCompat.START);
+       }
+       else {
+           super.onBackPressed();
+       }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        switch(item.getItemId()){
+            case R.id.appbarProfile :
+                //ModifyProfile Intent
+                return true;
+            case R.id.appbarSearchId :
+                //SearchActivity Intent
+                return true;
+            case R.id.appbarAddGroup :
+                //AddGroupActivity Intent
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
