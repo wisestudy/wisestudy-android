5a943e7a4710174ffc8cee70f13ee14925d0b8b1
jonghyunH <whdgus85845@gmail.com>
2020-06-13 17:27:18 +0900

    feat(#15) : 카카오 로그인 API 구현

--- app/src/main/java/com/wisestudy/login/activity/LoginActivity.java
+++ app/src/main/java/com/wisestudy/login/activity/LoginActivity.java
@@ -1,28 +1,95 @@
 package com.wisestudy.login.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.kakao.auth.ApiResponseCallback;
+import com.kakao.auth.AuthService;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.auth.network.response.AccessTokenInfoResponse;
+import com.kakao.network.ErrorResult;
+import com.kakao.util.exception.KakaoException;
 import com.wisestudy.user.activity.UserCreateActivity;
 import com.wisestudy.wisestudy.R;
 
-public class LoginActivity extends AppCompatActivity
-{
-    private Button loginButton;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import android.content.pm.Signature;
+
+import static com.kakao.util.helper.Utility.getPackageInfo;
+
+
+public class LoginActivity extends Activity {
+    // 세션 콜백 구현
+    private ISessionCallback sessionCallback = new ISessionCallback() {
+        @Override
+        public void onSessionOpened() {
+            Log.i("KAKAO_SESSION", "로그인 성공");
+
+            AuthService.getInstance()
+                    .requestAccessTokenInfo(new ApiResponseCallback<AccessTokenInfoResponse>() {
+                        @Override
+                        public void onSessionClosed(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                        }
+
+                        @Override
+                        public void onFailure(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "토큰 정보 요청 실패: " + errorResult);
+                        }
+
+                        @Override
+                        public void onSuccess(AccessTokenInfoResponse result) {
+                            Log.i("KAKAO_API", "사용자 아이디: " + result.getUserId());
+                            Log.i("KAKAO_API", "남은 시간 (ms): " + result.getExpiresInMillis());
+                            Intent intent = new Intent(getApplicationContext(),UserCreateActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.e("KAKAO_SESSION", "로그인 실패", exception);
+        }
+    };
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginButton =findViewById(R.id.loginBtn);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), UserCreateActivity.class);
-                startActivity(intent);
-            }
-        });
+
+        // 세션 콜백 등록
+        Session.getCurrentSession().addCallback(sessionCallback);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 세션 콜백 삭제
+        Session.getCurrentSession().removeCallback(sessionCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
