e46542fa516b0b92c2ccbdf5871095ce05ddb89d
leewoongi <t2korea@naver.com>
2020-08-02 16:49:51 +0900

    fear(#28) : refact

--- app/src/main/java/com/wisestudy/application/LoginInit.java
+++ app/src/main/java/com/wisestudy/application/LoginInit.java
@@ -1,4 +1,4 @@
-package com.wisestudy.login.activity;
+package com.wisestudy.application;
 
 import android.app.Application;
 import android.content.Context;
---
+++ app/src/main/java/com/wisestudy/user/retrofitapi/UserService.java
@@ -0,0 +1,11 @@
+package com.wisestudy.user.retrofitapi;
+
+import com.wisestudy.user.domain.UserVO;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+
+public interface UserService {
+
+
+}
