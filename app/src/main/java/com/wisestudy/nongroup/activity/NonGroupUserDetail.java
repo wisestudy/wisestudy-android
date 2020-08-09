b760d8531091de7d87d2cc91f0c72fe9db2ea61f
leewoongi <t2korea@naver.com>
2020-08-09 12:35:39 +0900

    feat(#28) : refact

---
+++ app/src/main/java/com/wisestudy/nongroup/activity/NonGroupUserDetail.java
@@ -0,0 +1,23 @@
+package com.wisestudy.nongroup.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.wisestudy.util.UiHelper;
+import com.wisestudy.wisestudy.R;
+
+public class NonGroupUserDetail extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_non_group_member);
+
+        UiHelper.toolBarInitialize(this, findViewById(R.id.nonGruopToolbar));
+        UiHelper.hideWindow(this);
+    }
+}
